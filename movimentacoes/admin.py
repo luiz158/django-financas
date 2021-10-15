25c4675ab6b49bfe228b9b6f474130ccae6f37a5
Thiago Brasil <tchaguitos@gmail.com>
2021-10-15 20:42:12 -0300

    :art: adiciona aplicativo de movimentacoes e modelos de categorias e movimentacoes

---
+++ movimentacoes/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from movimentacoes.models import Categoria, Movimentacao
+
+admin.site.register(Categoria)
+admin.site.register(Movimentacao)
---
+++ movimentacoes/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+
+
+class Tipo(models.IntegerChoices):
+    RECEITA = 1, "Receita"
+    DESPESA = 2, "Despesa"
+
+
+class Categoria(models.Model):
+    tipo = models.IntegerField(
+        choices=Tipo.choices,
+        default=Tipo.DESPESA,
+    )
+
+    nome = models.CharField(
+        verbose_name="Nome da categoria",
+        max_length=80,
+    )
+
+    class Meta:
+        db_table = "categoria"
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+
+    def __str__(self):
+        return self.nome
+
+
+class Movimentacao(models.Model):
+    tipo = models.IntegerField(
+        choices=Tipo.choices,
+        default=Tipo.DESPESA,
+    )
+
+    conta = models.ForeignKey(
+        "contas.Conta",
+        verbose_name="Conta em que a movimentação ocorreu",
+        on_delete=models.PROTECT,
+    )
+
+    categoria = models.ForeignKey(
+        Categoria,
+        verbose_name="Categoria da movimentação",
+        on_delete=models.PROTECT,
+    )
+
+    liquidada = models.BooleanField(
+        verbose_name="Despesa descontada/receita recebida?",
+        editable=False,
+        default=False,
+    )
+
+    descricao = models.CharField(
+        verbose_name="Descrição",
+        max_length=50,
+    )
+
+    valor = models.DecimalField(
+        verbose_name="Valor da movimentação",
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    data = models.DateField(
+        verbose_name="Data da movimentação"
+    )
+
+    class Meta:
+        db_table = "movimentacao"
+        verbose_name = "Movimentação"
+        verbose_name_plural = "Movimentações"
+
+    def __str__(self):
+        return f"R$ {self.valor} | {self.conta}"
